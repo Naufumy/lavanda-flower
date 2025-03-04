4baaf63f81af565b44d9fd1ae483b39972110b70
Naufumy <zatulovskiydv24@st.ithub.ru>
2025-03-04 18:45:50 +0300

    Добавил Cloudinary в requirements.txt

--- shop/models.py
+++ shop/models.py
@@ -6,7 +6,7 @@ class Flower(models.Model):
     name = models.CharField(max_length=100, verbose_name="Название")
     description = models.TextField(verbose_name="Описание")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
-    image = CloudinaryField('image', default='your_default_image_url')
+    image = CloudinaryField('image', null=True, blank=True)
 
 
     def __str__(self):
